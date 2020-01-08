2681e53376a6ccac85b5523e0d79c93239eab892
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-01-08 17:39:41 +0000

    first 2 pangram tests passed after much pain...

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,17 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
+  return this.alphabet.every((letter) => {
+    // console.log(letter);
+    return this.phrase.includes(letter) || letter === " ";
+  });
+};
 
-}
+// pangram = new PangramFinder('this is not a pangram so the result should be false')
+// console.log(pangram.phrase);
+// console.log(pangram.alphabet);
 
 module.exports = PangramFinder;
