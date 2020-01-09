7f48b5d65a189b4910a28334c09acb7ff864bded
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-01-09 08:40:01 +0000

    anagram finder 1 test passed

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,19 @@
 const AnagramFinder = function (word) {
-
+  this.word = word.split('')
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+  let isAnagram = false
+  const anagrams = []
+  otherWordsSplit = otherWords.map(word => word.split(''));
+  otherWordsSplit.filter((word) => {
+    if (word.every(letter => this.word.includes(letter))) {
+      joinedWord = word.join('');
+      anagrams.push(joinedWord);
+    }
+  });
+  return anagrams
+  // console.log(word);
 }
 
 module.exports = AnagramFinder;
